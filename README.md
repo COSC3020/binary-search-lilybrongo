23a975ea6aa5b8b0464a5b73428970142743330e
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-09-10 16:43:18 +0000

    First attempt, modifications to readme and code.js

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ not there.
 
 Use the template in `code.js`. The tests in `code.test.js` will be run
 automatically every time you push to Github; if they pass you are done.
+
+Worked/communicated with fellow students Daniel Collins and Will Greiner. Also reviwed what was required of a binary search through this link: https://www.programiz.com/dsa/binary-search
+
+
+I certify that I have listed all sources used to complete this exercise, including the use
+of any Large Language Models. All of the work is my own, except where stated
+otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is
+suspected, charges may be filed against me without prior notice
--- code.js
+++ code.js
@@ -1,24 +1,30 @@
 function binarySearch(list, element) {
-    // Creating variables in order to find the midpoint. 
-    // Will then iterate through the array after finding the midpoint 
-    // and continue to use the high and low points to sort through the 
+    // Initializing the needed variables in order to find the midpoint, then iterating through the list
+
     let low = 0;
     let high = list.length-1;
     let midpoint;
 
-    while (high >= low){
+    //This will calculate the midpoint that will then be used to compare to other elements
+    while (high <= low){
         midpoint = low + Math.floor((high-low)/2);
 
-        if(list[mid] == x)
-            return mid;
+        //Return the midpoint if it is equal to the element
+        if(list[midpoint] === element){
+            return midpoint;
+        }    
 
-        if(list[mid] > x)
-            high = mid - 1;
+        //Adjusting the midpoint if the element is less than
+        else if(list[midpoint] > element){
+            high = midpoint - 1;
+        }
 
-        else
-            low = mid - 1;
+        //Adjusting the midpoint if the element is greater than
+        else{
+            low = midpoint - 1;
+        }    
     }
 
-
+    //Exits loop if element is not found
     return -1;
 }
