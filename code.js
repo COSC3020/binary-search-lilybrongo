7906e0531e266a7acca20291d5d7f2939b5e9015
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-09-24 11:02:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -15,12 +15,12 @@ function binarySearch(list, element) {
         }    
 
         //Adjusting the midpoint if the element is less than
-        else (list[midpoint] > element){
+        else if(list[midpoint] > element){
             high = midpoint - 1;
         }
 
         //Adjusting the midpoint if the element is greater than
-        else if{
+        else {
             low = midpoint + 1;
         }    
     }
